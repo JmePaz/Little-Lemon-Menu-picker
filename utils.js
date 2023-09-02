34c5ed948ebcdcdce5acec0010145c4791ae5dc8
James Michael E. Paz <105730089+JmePaz@users.noreply.github.com>
2023-09-02 21:51:25 +0800

    Initial commit

---
+++ utils.js
@@ -0,0 +1,82 @@
+import { useRef, useEffect } from 'react';
+
+export const SECTION_LIST_MOCK_DATA = [
+    {
+      title: 'Appetizers',
+      data: [
+        {
+          id: '1',
+          title: 'Pasta',
+          price: '10',
+        },
+        {
+          id: '3',
+          title: 'Pizza',
+          price: '8',
+        },
+      ],
+    },
+    {
+      title: 'Salads',
+      data: [
+        {
+          id: '2',
+          title: 'Caesar',
+          price: '2',
+        },
+        {
+          id: '4',
+          title: 'Greek',
+          price: '3',
+        },
+      ],
+    },
+  ];
+
+/**
+ * 3. Implement this function to transform the raw data
+ * retrieved by the getMenuItems() function inside the database.js file
+ * into the data structure a SectionList component expects as its "sections" prop.
+ * @see https://reactnative.dev/docs/sectionlist as a reference
+ */
+export function getSectionListData(data) {
+  // SECTION_LIST_MOCK_DATA is an example of the data structure you need to return from this function.
+  // The title of each section should be the category.
+  // The data property should contain an array of menu items. 
+  // Each item has the following properties: "id", "title" and "price"
+  const formatted = data.reduce((acc, elem)=>{
+    if(elem ==null)
+      return acc
+    
+    const item = {id:elem.id, title: elem.title, price: elem.price }
+    if(!(elem.category in acc))
+        acc[elem.category] = [item]
+    else{
+      acc[elem.category].push(item)
+    }
+    return acc
+  }, {})
+  
+  const formatted2 =  Object.entries(formatted).reduce((
+    acc, elem
+  )=>{
+    acc.push({
+      title: elem[0],
+      data: elem[1]
+    })
+    return acc
+  },  [])
+  return formatted2;
+}
+
+export function useUpdateEffect(effect, dependencies = []) {
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      return effect();
+    }
+  }, dependencies);
+}
